8ecf24b68cda66ecc9827b76d543feaa1af3ce1f
Khushi Patel <khushipatel140904@gmail.com>
2026-02-16 23:20:42 +0530

    16 Feb

--- C.cpp
+++ C.cpp
@@ -5,8 +5,22 @@ int main(){
 
     //DATATYPES- used to specify which type of value are used toh define the variable 
     //int - 4 bytes
-    //char - 
+    //char - 1 byte
+    //float - 4 bytes
+    //bool-1 byte
+    //double- 8 byte
+
+    int age =10;
+    char vowel = 'a';
+    float dec = 3.4f; //f nahi lagya toh matlab double haii
+    bool flag = true;
+    double dec1=33.456;
     
-    cout<<"hello world..."<<endl;
+    cout<<age<<endl;
+    cout<<vowel<<endl;
+    cout<<dec<<endl;
+    cout<<flag<<endl;
+    cout<<dec1<<endl;
+
     return 0;
 }
---
+++ D.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    /*Operators
+    1.arithemetic - + - * / % 
+    
+    2.Relational - <,<=,>,>=,==,!= ----> these operators return true/false 
+    
+    3.logical - OR ||
+                AND &&
+                NOT !
+    */
+
+    cout<<(3>5)<<endl;
+    cout<<3+8<<endl;
+    cout<<!(3<5)<<endl;
+
+    return 0;
+}
